5294532bd5bce1b8323fa78da59131d07d84025b
Carlos Camara <Carlos.Camara@warwick.ac.uk>
2024-06-26 13:22:11 +0100

    Add and documents function

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -1,3 +1,16 @@
+#' Prints what a given animal says
+#'
+#' Prints what the animal says *(the sound it makes)* in the console, using
+#' [`paste0()`]
+#'
+#' @param animal a string containing an animal name
+#' @param sound  a string containing the sound that the animal makes
+#'
+#' @return a printed versiib
+#' @export
+#'
+#' @examples
+#' animal_sounds("dog", "woof")
 animal_sounds <- function(animal, sound) {
   stopifnot(is.character(animal) & length(animal) == 1)
   stopifnot(is.character(sound) & length(sound) == 1)
