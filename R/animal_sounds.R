438405fb25a7c4a445857e6140e7c18d72f1c641
Carlos Camara <Carlos.Camara@warwick.ac.uk>
2024-06-26 15:41:01 +0100

    Add tests

--- R/animal_sounds.R
+++ R/animal_sounds.R
@@ -11,22 +11,30 @@
 #'
 #' @examples
 #' animal_sounds("dog", "woof")
-animal_sounds <- function(animal, sound) {
+animal_sounds <- function(animal, sound = NULL) {
 
   if (!rlang::is_character(animal, n = 1)) {
     cli::cli_abort(
       c("{.var animal} must be a single string!",
-        "i" = "It was {.type {animal}} of length {length(animal)} instead.")
+        "i" = "It was {.type {animal}} of length {length(animal)} instead."),
+      class = "error_not_single_string"
     )
   }
 
-  if (!rlang::is_character(sound, n = 1)) {
+  if (!rlang::is_null(sound) && !rlang::is_character(sound, n = 1)   ) {
     cli::cli_abort(
-      c("{.var sound} must be a single string!",
-        "i" = "It was {.type {sound}} of length {length(sound)} instead.")
+      c("{.var sound} must be a single string or empty!",
+        "i" = "It was {.type {sound}} of length {length(sound)} instead."),
+      class = "error_not_single_string"
     )
   }
 
+  if (is.null(sound)) {
+    paste0("The ", animal, " makes no sound.")
+  } else(
+    paste0("The ", animal, " goes ", sound, "!")
+  )
 
-  paste0("The ", animal, " goes ", sound, "!")
+
+
 }
---
+++ tests/testthat/test-animal_sounds.R
@@ -0,0 +1,25 @@
+test_that("animal_sounds produces expected strings", {
+  dog_woof <- animal_sounds("dog", "woof")
+  expect_equal(dog_woof, "The dog goes woof!")
+  expect_equal(animal_sounds("cat", "miaow"), "The cat goes miaow!")
+})
+
+test_that("handles invalid inputs", {
+  expect_error(animal_sounds("dog", c("woof", "bow wow wow")),
+               class = "error_not_single_string")
+  expect_error(animal_sounds(factor("cat"), "miaow"),
+               class = "error_not_single_string")
+})
+
+# Snapshots
+test_that("error message for invalid input", {
+  expect_snapshot(animal_sounds("dog", c("woof", "bow wow wow")),
+                  error = TRUE)
+})
+
+# Test driven development
+test_that("optional sound argument", {
+  giraffe <- animal_sounds("giraffe")
+  expect_equal(giraffe,
+               "The giraffe makes no sound.")
+})
